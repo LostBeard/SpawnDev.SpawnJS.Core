d789da90fc3ec7b3e0b4afd6a5c2f35b7d37bafa
Todd Tanner <todd@spawndev.com>
2026-08-14 18:44:05 -0400

    Added ITuple Marshaller

---
+++ SpawnDev.SpawnJS.Core/Marshallers/ITupleMarshaller.cs
@@ -0,0 +1,59 @@
+﻿using SpawnDev.SpawnJS.Marshaller;
+using System.Runtime.CompilerServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SpawnDev.SpawnJS.Marshallers
+{
+    public class ITupleMarshaller<TTuple> : JSMarshallerFromSpawnJSObjectReference<TTuple> where TTuple : ITuple
+    {
+        Type TypeT;
+        Type[] GenericTypes;
+        public ITupleMarshaller()
+        {
+            TypeT = typeof(TTuple); ;
+            GenericTypes = TypeT.GenericTypeArguments;
+        }
+        public override TTuple JSToNet(SpawnJSObjectReference value)
+        {
+            if (value == null) return default!;
+            var list = new object?[GenericTypes.Length];
+            for (var i = 0; i < GenericTypes.Length; i++)
+            {
+                list[i] = ((Delegate)readTyped<object>).InvokeGeneric(GenericTypes[i]);
+                T readTyped<T>() => value.Get<T>(i);
+            }
+            var ret = (TTuple)Activator.CreateInstance(TypeT, list)!;
+            return ret;
+        }
+        public override void NetToJS(SpawnJSObjectReference jsParent, int jsKey, TTuple value)
+        {
+            if (value == null)
+            {
+                jsParent.PropertySetNull(jsKey);
+                return;
+            }
+            using var array = JS.New("Array");
+            for (var i = 0; i < value.Length; i++)
+            {
+                var item = value[i];
+                array.Set(i, item);
+            }
+            jsParent.PropertySet(jsKey, array);
+        }
+        public override void NetToJS(SpawnJSObjectReference jsParent, string jsKey, TTuple value)
+        {
+            if (value == null)
+            {
+                jsParent.PropertySetNull(jsKey);
+                return;
+            }
+            using var array = JS.New("Array");
+            for (var i = 0; i < value.Length; i++)
+            {
+                var item = value[i];
+                array.Set(i, item);
+            }
+            jsParent.PropertySet(jsKey, array);
+        }
+    }
+}
---
+++ SpawnDev.SpawnJS.Core/Marshallers/ITupleMarshallerFactory.cs
@@ -0,0 +1,57 @@
+﻿using SpawnDev.SpawnJS.Marshaller;
+
+namespace SpawnDev.SpawnJS.Marshallers
+{
+    public class ITupleMarshallerFactory : JSMarshaller
+    {
+        public override ReturnType ReturnType => throw new NotImplementedException();
+        static List<Type> SupportedGenericTypes = new List<Type> {
+            { typeof(ValueTuple<>) },
+            { typeof(ValueTuple<,>) },
+            { typeof(ValueTuple<,,>) },
+            { typeof(ValueTuple<,,,>) },
+            { typeof(ValueTuple<,,,,>) },
+            { typeof(ValueTuple<,,,,,>) },
+            { typeof(ValueTuple<,,,,,,>) },
+            { typeof(Tuple<>) },
+            { typeof(Tuple<,>) },
+            { typeof(Tuple<,,>) },
+            { typeof(Tuple<,,,>) },
+            { typeof(Tuple<,,,,>) },
+            { typeof(Tuple<,,,,,>) },
+            { typeof(Tuple<,,,,,,>) },
+        };
+
+        public override bool CanMarshal(Type type)
+        {
+            // ValueType is a value type and may be wrapped in Nullable<> if it is nullable(?)
+            var genericType = type.IsGenericType ? type.GetGenericTypeDefinition() : null;
+            if (genericType == typeof(Nullable<>))
+            {
+                type = type.GenericTypeArguments[0];
+                genericType = type.IsGenericType ? type.GetGenericTypeDefinition() : null;
+            }
+            return genericType != null && SupportedGenericTypes.Contains(genericType);
+        }
+        public override JSMarshaller<T> GetMarshaller<[System.Diagnostics.CodeAnalysis.DynamicallyAccessedMembers(System.Diagnostics.CodeAnalysis.DynamicallyAccessedMemberTypes.PublicConstructors)] T>()
+        {
+            var marshallerTyped = typeof(ITupleMarshaller<>).MakeGenericType(typeof(T));
+            return (JSMarshaller<T>)Activator.CreateInstance(marshallerTyped)!;
+        }
+        ///// <summary>
+        ///// Creates a JsonConverter instance
+        ///// </summary>
+        //public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+        //{
+        //    var isNullable = false;
+        //    var genericType = typeToConvert.IsGenericType ? typeToConvert.GetGenericTypeDefinition() : null;
+        //    if (genericType == typeof(Nullable<>))
+        //    {
+        //        isNullable = true;
+        //        typeToConvert = typeToConvert.GenericTypeArguments[0];
+        //        genericType = typeToConvert.IsGenericType ? typeToConvert.GetGenericTypeDefinition() : null;
+        //    }
+        //    return new ITupleMarshaller(typeToConvert, genericType!, isNullable);
+        //}
+    }
+}
