a2ef378acd753488577d78f65612133ed50929c0
Todd Tanner <todd@spawndev.com>
2026-08-14 22:32:03 -0400

    Added ToolBox items

---
+++ SpawnDev.SpawnJS.Core/Toolbox/Async.cs
@@ -0,0 +1,21 @@
+﻿namespace SpawnDev.SpawnJS.Toolbox
+{
+    /// <summary>
+    /// Helper methods for running async lambda methods
+    /// </summary>
+    public static class Async
+    {
+        /// <summary>
+        /// Shorthand wrapper for running async lambda functions
+        /// </summary>
+        public static void Run(Func<Task> func) => _ = func();
+        /// <summary>
+        /// Shorthand wrapper for running async lambda functions
+        /// </summary>
+        public static Task RunAsync(Func<Task> func) => func();
+        /// <summary>
+        /// Shorthand wrapper for running async lambda functions
+        /// </summary>
+        public static Task<T> RunAsync<T>(Func<Task<T>> func) => func();
+    }
+}
